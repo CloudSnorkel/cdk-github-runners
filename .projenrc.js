CloudSnorkel/cdk-github-runners

commit 6d320018f417fa974a8633d8f8af75497b8780e0

fix: Failure to update runner if image hasn't changed (#103)
This affected Lambda runners and Windows runners.
We create images as part of the stack, but we also update them on a schedule and delete the old ones. That means any stack update that touches just the runner and not the image will fail. It will fail because the Lambda function or CodeBuild project will be updated, but the stack will still try to update it with the old image as it's not aware of the new image. This code forces the stack to always get the latest image digest for Lambda, even if the image wasn't created by the stack. On the Windows side, we just tell it to use the 'latest' tag, so we don't have to worry about this issue. That means it's now just a Lambda runner problem and can be confined in the Lambda runner code. Lambda is the only runner provider that needs a digest instead of a tag.

BREAKING CHANGE: RunnerImage.imageDigest was removed

src/providers/lambda.ts
@@ -1,7 +1,6 @@
 import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import {
-  Annotations,
   aws_ec2 as ec2,
   aws_events as events,
   aws_events_targets as events_targets,
@@ -9,6 +8,7 @@ import {
   aws_lambda as lambda,
   aws_stepfunctions as stepfunctions,
   aws_stepfunctions_tasks as stepfunctions_tasks,
+  custom_resources as cr,
 } from 'aws-cdk-lib';
 import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
@@ -164,16 +164,22 @@ export class LambdaRunner extends Construct implements IRunnerProvider {
       throw new Error(`Unable to find support Lambda architecture for ${image.os.name}/${image.architecture.name}`);
     }
 
-    let code;
-    if (image.imageDigest) {
-      code = lambda.DockerImageCode.fromEcr(image.imageRepository, { tagOrDigest: `sha256:${image.imageDigest}` });
-    } else {
-      if (image.imageTag == 'latest') {
-        Annotations.of(this).addWarning('imageTag is `latest` even though imageDigest is not specified! This means any updates to the image by the' +
-          'stack will be used.');
-      }
-      code = lambda.DockerImageCode.fromEcr(image.imageRepository, { tagOrDigest: image.imageTag });
-    }
+    // get image digest and make sure to get it every time the lambda function might be updated
+    // pass all variables that may change and cause a function update
+    // if we don't get the latest digest, the update may fail as a new image was already built outside the stack on a schedule
+    // we automatically delete old images, so we must always get the latest digest
+    const imageDigest = this.imageDigest(image, {
+      version: 1, // bump this for any non-user changes like description or defaults
+      label: this.label,
+      architecture: architecture.name,
+      vpc: this.vpc?.vpcId,
+      securityGroups: this.securityGroup?.securityGroupId,
+      vpcSubnets: props.subnetSelection?.subnets?.map(s => s.subnetId),
+      timeout: props.timeout?.toSeconds(),
+      memorySize: props.memorySize,
+      ephemeralStorageSize: props.ephemeralStorageSize?.toKibibytes(),
+      logRetention: props.logRetention?.toFixed(),
+    });
 
     this.function = new lambda.DockerImageFunction(
       this,
@@ -181,7 +187,7 @@ export class LambdaRunner extends Construct implements IRunnerProvider {
       {
         description: `GitHub Actions runner for "${this.label}" label`,
         // CDK requires "sha256:" literal prefix -- https://github.com/aws/aws-cdk/blob/ba91ca45ad759ab5db6da17a62333e2bc11e1075/packages/%40aws-cdk/aws-ecr/lib/repository.ts#L184
-        code,
+        code: lambda.DockerImageCode.fromEcr(image.imageRepository, { tagOrDigest: `sha256:${imageDigest}` }),
         architecture,
         vpc: this.vpc,
         securityGroups: this.securityGroup && [this.securityGroup],
@@ -281,5 +287,62 @@ export class LambdaRunner extends Construct implements IRunnerProvider {
 
     // the event never triggers without this - not sure why
     (rule.node.defaultChild as events.CfnRule).addDeletionOverride('Properties.EventPattern.resources');
+  }
+
+  private imageDigest(image: RunnerImage, variableSettings: any): string {
+    // describe ECR image to get its digest
+    // the physical id is random so the resource always runs and always gets the latest digest, even if a scheduled build replaced the stack image
+    const reader = new cr.AwsCustomResource(this, 'Image Digest Reader', {
+      onCreate: {
+        service: 'ECR',
+        action: 'describeImages',
+        parameters: {
+          repositoryName: image.imageRepository.repositoryName,
+          imageIds: [
+            {
+              imageTag: image.imageTag,
+            },
+          ],
+        },
+        physicalResourceId: cr.PhysicalResourceId.of('ImageDigest'),
+      },
+      onUpdate: {
+        service: 'ECR',
+        action: 'describeImages',
+        parameters: {
+          repositoryName: image.imageRepository.repositoryName,
+          imageIds: [
+            {
+              imageTag: image.imageTag,
+            },
+          ],
+        },
+        physicalResourceId: cr.PhysicalResourceId.of('ImageDigest'),
+      },
+      onDelete: {
+        // this will NOT be called thanks to RemovalPolicy.RETAIN below
+        // we only use this to force the custom resource to be called again and get a new digest
+        service: 'fake',
+        action: 'fake',
+        parameters: variableSettings,
+      },
+      policy: cr.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [image.imageRepository.repositoryArn],
+      }),
+      resourceType: 'Custom::EcrImageDigest',
+      installLatestAwsSdk: false, // no need and it takes 60 seconds
+      logRetention: RetentionDays.ONE_MONTH,
+    });
+
+    const res = reader.node.tryFindChild('Resource') as cdk.CustomResource | undefined;
+    if (res) {
+      // don't actually call the fake onDelete above
+      res.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN);
+    } else {
+      throw new Error('Resource not found in AwsCustomResource. Report this bug at https://github.com/CloudSnorkel/cdk-github-runners/issues.');
+    }
+
+    // return only the digest because CDK expects 'sha256:' literal above
+    return cdk.Fn.split(':', reader.getResponseField('imageDetails.0.imageDigest'), 2)[1];
   }
 }

src/providers/common.ts
@@ -100,15 +100,6 @@ export interface RunnerImage {
   readonly imageTag: string;
 
   /**
-   * Image digest for providers that need to know the digest like Lambda.
-   *
-   * If the digest is not specified, imageTag must always point to a new tag on update. If not, the build may try to use the old image.
-   *
-   * WARNING: the digest might change when the builder automatically rebuilds the image on a schedule. Do not expect for this digest to stay the same between deploys.
-   */
-  readonly imageDigest?: string;
-
-  /**
    * Architecture of the image.
    */
   readonly architecture: Architecture;

src/providers/image-builders/codebuild.ts
@@ -147,7 +147,11 @@ export interface CodeBuildImageBuilderProps {
  * ```
  */
 export class CodeBuildImageBuilder extends Construct implements IImageBuilder {
-  private static BUILDSPEC_VERSION = 1;
+  /**
+   * Bump this number every time the buildspec or any important setting of the project changes. It will force a rebuild of the image.
+   * @private
+   */
+  private static BUILDSPEC_VERSION = 2;
 
   private readonly architecture: Architecture;
   private readonly os: Os;
@@ -340,13 +344,12 @@ export class CodeBuildImageBuilder extends Construct implements IImageBuilder {
 
     this.boundImage = {
       imageRepository: ecr.Repository.fromRepositoryAttributes(this, 'Dependable Image', {
-        repositoryName: this.repository.repositoryName,
-        // There are simpler ways to get the ARN, but we want an image object that depends on the custom resource.
+        // There are simpler ways to get name and ARN, but we want an image object that depends on the custom resource.
         // We want whoever is using this image to automatically wait for CodeBuild to start and finish through the custom resource.
+        repositoryName: cr.getAttString('Name'),
         repositoryArn: cr.ref,
       }),
       imageTag: 'latest',
-      imageDigest: cr.getAtt('Digest').toString(),
       architecture: this.architecture,
       os: this.os,
       logGroup,
@@ -406,8 +409,7 @@ export class CodeBuildImageBuilder extends Construct implements IImageBuilder {
         post_build: {
           commands: this.postBuild.concat([
             'STATUS="SUCCESS"',
-            'DIGEST="UNKNOWN"',
-            'if [ $CODEBUILD_BUILD_SUCCEEDING -ne 1 ]; then STATUS="FAILED"; else DIGEST=`docker inspect "$REPO_URI" | jq -r \'.[0].RepoDigests[0] | split("@")[1] | split(":")[1]\'`; fi',
+            'if [ $CODEBUILD_BUILD_SUCCEEDING -ne 1 ]; then STATUS="FAILED"; fi',
             'cat <<EOF > /tmp/payload.json\n' +
             '{\n' +
             '  "StackId": "$STACK_ID",\n' +
@@ -416,7 +418,7 @@ export class CodeBuildImageBuilder extends Construct implements IImageBuilder {
             '  "PhysicalResourceId": "$REPO_ARN",\n' +
             '  "Status": "$STATUS",\n' +
             `  "Reason": "See logs in ${logGroup.logGroupName}/$CODEBUILD_LOG_PATH (deploy again with \'cdk deploy -R\' or logRemovalPolicy=RemovalPolicy.RETAIN if they are already deleted)",\n` +
-            '  "Data": {"Digest": "$DIGEST"}\n' + // include the digest to mark the resource updated so the runner providers get updated with the latest digest too (specifically Lambda)
+            `  "Data": {"Name": "${repository.repositoryName}"}\n` +
             '}\n' +
             'EOF',
             'if [ "$RESPONSE_URL" != "unspecified" ]; then jq . /tmp/payload.json; curl -fsSL -X PUT -H "Content-Type:" -d "@/tmp/payload.json" "$RESPONSE_URL"; fi',

src/providers/image-builders/container.ts
@@ -669,7 +669,7 @@ export class ContainerImageBuilder extends Construct implements IImageBuilder {
         // we can't use image.attrName because it comes up with upper case
         cdk.Fn.split(':', cdk.Fn.split('/', image.attrImageUri, 2)[1], 2)[0],
       ),
-      imageTag: cdk.Fn.split(':', image.attrImageUri, 2)[1],
+      imageTag: 'latest',
       os: this.os,
       architecture: this.architecture,
       logGroup: log,

commit 3a955b6a8f583a394f8b456b98cda61915022626

fix: Lambda image not updating while stack is deployed (#116)
Use a simpler method to avoid conflicts when updating the function code after the image is updated. Instead of looking for a running stack operation, just try again in a loop every 10 seconds. The current code assumed some special handling in CloudFormation that would update the function later. It wasn't working. I believe it was broken by #103.

.projenrc.js
@@ -20,6 +20,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     '@octokit/rest',
     'aws-sdk',
     '@aws-sdk/types',
+    '@aws-sdk/client-lambda',
     '@types/aws-lambda',
     'semver',
     '@types/semver',

src/lambdas/update-lambda/index.ts
@@ -1,36 +1,39 @@
 /* eslint-disable-next-line import/no-extraneous-dependencies */
+import { ResourceConflictException } from '@aws-sdk/client-lambda';
+/* eslint-disable-next-line import/no-extraneous-dependencies */
 import * as AWS from 'aws-sdk';
 
-const cfn = new AWS.CloudFormation();
 const lambda = new AWS.Lambda();
 
 interface Input {
   readonly lambdaName: string;
   readonly repositoryUri: string;
   readonly repositoryTag: string;
-  readonly stackName: string;
+}
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 export async function handler(event: Input) {
   console.log(JSON.stringify(event));
 
-  const stacks = await cfn.describeStacks({
-    StackName: event.stackName,
-  }).promise();
-
-  if (stacks.Stacks?.length != 1) {
-    console.error(`Unable to find stack ${event.stackName}`);
-    return;
+  while (true) {
+    try {
+      await lambda.updateFunctionCode({
+        FunctionName: event.lambdaName,
+        ImageUri: `${event.repositoryUri}:${event.repositoryTag}`,
+        Publish: true,
+      }).promise();
+      break;
+    } catch (e) {
+      if (e instanceof ResourceConflictException) {
+        // keep trying if function is already being updated by CloudFormation
+        // this can happen if we update some settings on the function and the image code at the same time
+        await sleep(10000);
+      } else {
+        throw e;
+      }
+    }
   }
-
-  if (stacks.Stacks[0].StackStatus.endsWith('_IN_PROGRESS')) {
-    console.error(`Stack ${event.stackName} is already in progress, skipping Lambda update as the stack will do it for us. If we update here, it may conflict with the stack operation.`);
-    return;
-  }
-
-  await lambda.updateFunctionCode({
-    FunctionName: event.lambdaName,
-    ImageUri: `${event.repositoryUri}:${event.repositoryTag}`,
-    Publish: true,
-  }).promise();
 }

src/providers/lambda.ts
@@ -240,21 +240,9 @@ export class LambdaRunner extends Construct implements IRunnerProvider {
     // Lambda needs to be pointing to a specific image digest and not just a tag.
     // Whenever we update the tag to a new digest, we need to update the lambda.
 
-    let stack = cdk.Stack.of(this);
-
     const updater = BundledNodejsFunction.singleton(this, 'update-lambda', {
       description: 'Function that updates a GitHub Actions runner function with the latest image digest after the image has been rebuilt',
-      timeout: cdk.Duration.seconds(30),
-      initialPolicy: [
-        new iam.PolicyStatement({
-          actions: ['cloudformation:DescribeStacks'],
-          resources: [stack.formatArn({
-            service: 'cloudformation',
-            resource: 'stack',
-            resourceName: `${stack.stackName}/*`,
-          })],
-        }),
-      ],
+      timeout: cdk.Duration.minutes(15),
     });
 
     updater.addToRolePolicy(new iam.PolicyStatement({
@@ -267,7 +255,6 @@ export class LambdaRunner extends Construct implements IRunnerProvider {
         lambdaName: this.function.functionName,
         repositoryUri: image.imageRepository.repositoryUri,
         repositoryTag: image.imageTag,
-        stackName: stack.stackName,
       }),
     });
 