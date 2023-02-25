e4ebc40016d22f31a3555b74c74797fdb6a99e1f
Amir Szekely <kichik@gmail.com>
2023-02-24 21:40:24 -0800

    fix: Clean-up Lambda /tmp directory for a fresh runner (#244)
    The `/tmp` directory can have some files left behind from previous runs when a Lambda instance is reused. We want a clean home directory and clean `/tmp` directory so one run can break the next one.
    
    See #242

--- src/providers/docker-images/lambda/linux-x64/runner.sh
+++ src/providers/docker-images/lambda/linux-x64/runner.sh
@@ -2,16 +2,22 @@
 
 set -e -u -o pipefail
 
+# cleanup
+find /tmp -mindepth 1 -maxdepth 1 -exec rm -rf '{}' \;
+# copy runner code (it needs a writable directory)
 cp -r /runner /tmp/
 cd /tmp/runner
-mkdir -p /tmp/home
+# setup home directory
+mkdir /tmp/home
 export HOME=/tmp/home
 
+# start runner
 if [ "${RUNNER_VERSION}" = "latest" ]; then RUNNER_FLAGS=""; else RUNNER_FLAGS="--disableupdate"; fi
 ./config.sh --unattended --url "https://${GITHUB_DOMAIN}/${OWNER}/${REPO}" --token "${RUNNER_TOKEN}" --ephemeral --work _work --labels "${RUNNER_LABEL}" --name "${RUNNER_NAME}" ${RUNNER_FLAGS}
 echo Config done
 ./run.sh
 echo Run done
 
+# print status for metrics
 STATUS=$(grep -Phors "finish job request for job [0-9a-f\-]+ with result: \K.*" _diag/ | tail -n1)
 [ -n "$STATUS" ] && echo CDKGHA JOB DONE "$RUNNER_LABEL" "$STATUS"
