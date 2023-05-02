cec8740f4e1d5e6e9e2ec251055a26c2270ff9be
Amir Szekely <kichik@gmail.com>
2023-05-02 15:06:35 -0700

    fix: Don't ignore `allowedIps` with `LambdaAccess.apiGateway({allowsVpc: vpc})`

--- test/access.test.ts
+++ test/access.test.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_ec2 as ec2 } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { GitHubRunners, LambdaAccess, LambdaRunnerProvider } from '../src';
 
@@ -31,7 +32,7 @@ test('No access', () => {
   template.resourceCountIs('AWS::ApiGateway::RestApi', 0);
 });
 
-test('Lamnda URL access', () => {
+test('Lambda URL access', () => {
   const app = new cdk.App();
   const stack = new cdk.Stack(app, 'test');
 
@@ -63,4 +64,47 @@ test('API Gateway access', () => {
 
   template.resourceCountIs('AWS::Lambda::Url', 0);
   template.resourceCountIs('AWS::ApiGateway::RestApi', 3);
+});
+
+test('Private API Gateway access', () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, 'test');
+
+  const vpc = new ec2.Vpc(stack, 'vpc');
+  const sg = new ec2.SecurityGroup(stack, 'sg', { vpc });
+
+  new GitHubRunners(stack, 'runners', {
+    setupAccess: LambdaAccess.apiGateway(),
+    webhookAccess: LambdaAccess.apiGateway({
+      allowedVpc: vpc,
+      allowedSecurityGroups: [sg],
+      allowedIps: ['1.2.3.4/32', '2002::1234:abcd:ffff:c0a8:101/64'],
+    }),
+    statusAccess: LambdaAccess.apiGateway(),
+    providers: [new LambdaRunnerProvider(stack, 'lambda')],
+  });
+
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs('AWS::Lambda::Url', 0);
+  template.resourceCountIs('AWS::ApiGateway::RestApi', 3);
+  template.resourceCountIs('AWS::EC2::VPCEndpoint', 1);
+  template.hasResourceProperties('AWS::EC2::SecurityGroup', {
+    SecurityGroupIngress: [
+      {
+        CidrIp: '1.2.3.4/32',
+        Description: 'from 1.2.3.4/32:443',
+        FromPort: 443,
+        IpProtocol: 'tcp',
+        ToPort: 443,
+      },
+      {
+        CidrIpv6: '2002::1234:abcd:ffff:c0a8:101/64',
+        Description: 'from 2002::1234:abcd:ffff:c0a8:101/64:443',
+        FromPort: 443,
+        IpProtocol: 'tcp',
+        ToPort: 443,
+      },
+    ],
+  });
 });
