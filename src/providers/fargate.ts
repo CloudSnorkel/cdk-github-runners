CloudSnorkel/cdk-github-runners

commit 956536f1fced3ed08e75de075592d147849965d8

feat: Runner group option (#630)
Allow setting [runner group](https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners/managing-access-to-self-hosted-runners-using-groups). GitHub Teams and Enterprise allow you to create runner groups and assign them to specific repos. If a repo is not assigned to the group, it can't use it. This allows you to limit certain runners to certain repos.

```
new CodeBuildRunnerProvider(this, 'limited runner', {
   labels: ['special-runner'],
   group: 'special',
});
```

The group must exist or the runner will not start. The following error will show up in the logs:

```
Could not find any self-hosted runner group named "special".
```

Users will still be able to trigger this runner with the correct labels. But the runner will only be able to run jobs from repos allowed to use the group.

Resolves #500

src/providers/fargate.ts
@@ -58,6 +58,20 @@ export interface FargateRunnerProviderProps extends RunnerProviderProps {
   readonly labels?: string[];
 
   /**
+   * GitHub Actions runner group name.
+   *
+   * If specified, the runner will be registered with this group name. Setting a runner group can help managing access to self-hosted runners. It
+   * requires a paid GitHub account.
+   *
+   * The group must exist or the runner will not start.
+   *
+   * Users will still be able to trigger this runner with the correct labels. But the runner will only be able to run jobs from repos allowed to use the group.
+   *
+   * @default undefined
+   */
+  readonly group?: string;
+
+  /**
    * VPC to launch the runners in.
    *
    * @default default account VPC
@@ -212,7 +226,7 @@ export function ecsRunCommand(os: Os, dind: boolean): string[] {
       `${dindCommand}
         cd /home/runner &&
         if [ "$RUNNER_VERSION" = "latest" ]; then RUNNER_FLAGS=""; else RUNNER_FLAGS="--disableupdate"; fi &&
-        ./config.sh --unattended --url "$REGISTRATION_URL" --token "$RUNNER_TOKEN" --ephemeral --work _work --labels "$RUNNER_LABEL,cdkghr:started:\`date +%s\`" $RUNNER_FLAGS --name "$RUNNER_NAME" &&
+        ./config.sh --unattended --url "$REGISTRATION_URL" --token "$RUNNER_TOKEN" --ephemeral --work _work --labels "$RUNNER_LABEL,cdkghr:started:\`date +%s\`" $RUNNER_FLAGS --name "$RUNNER_NAME" $RUNNER_GROUP &&
         ./run.sh &&
         STATUS=$(grep -Phors "finish job request for job [0-9a-f\\-]+ with result: \\K.*" _diag/ | tail -n1) &&
         [ -n "$STATUS" ] && echo CDKGHA JOB DONE "$RUNNER_LABEL" "$STATUS"`,
@@ -222,7 +236,7 @@ export function ecsRunCommand(os: Os, dind: boolean): string[] {
       'powershell', '-Command',
       `cd \\actions ;
         if ($Env:RUNNER_VERSION -eq "latest") { $RunnerFlags = "" } else { $RunnerFlags = "--disableupdate" } ;
-        ./config.cmd --unattended --url "\${Env:REGISTRATION_URL}" --token "\${Env:RUNNER_TOKEN}" --ephemeral --work _work --labels "\${Env:RUNNER_LABEL},cdkghr:started:\$(Get-Date -UFormat +%s)" $RunnerFlags --name "\${Env:RUNNER_NAME}" ;
+        ./config.cmd --unattended --url "\${Env:REGISTRATION_URL}" --token "\${Env:RUNNER_TOKEN}" --ephemeral --work _work --labels "\${Env:RUNNER_LABEL},cdkghr:started:\$(Get-Date -UFormat +%s)" $RunnerFlags --name "\${Env:RUNNER_NAME} \${Env:RUNNER_GROUP}" ;
         ./run.cmd ;
         $STATUS = Select-String -Path './_diag/*.log' -Pattern 'finish job request for job [0-9a-f\\-]+ with result: (.*)' | %{$_.Matches.Groups[1].Value} | Select-Object -Last 1 ;
         if ($STATUS) { echo "CDKGHA JOB DONE $\{Env:RUNNER_LABEL\} $STATUS" }`,
@@ -363,12 +377,14 @@ export class FargateRunnerProvider extends BaseProvider implements IRunnerProvid
     'Ecs.UpdateInProgressException',
   ];
 
+  private readonly group?: string;
   private readonly securityGroups: ec2.ISecurityGroup[];
 
   constructor(scope: Construct, id: string, props?: FargateRunnerProviderProps) {
     super(scope, id, props);
 
     this.labels = this.labelsFromProperties('fargate', props?.label, props?.labels);
+    this.group = props?.group;
     this.vpc = props?.vpc ?? ec2.Vpc.fromLookup(this, 'default vpc', { isDefault: true });
     this.subnetSelection = props?.subnetSelection;
     this.securityGroups = props?.securityGroup ? [props.securityGroup] : (props?.securityGroups ?? [new ec2.SecurityGroup(this, 'security group', { vpc: this.vpc })]);
@@ -453,7 +469,7 @@ export class FargateRunnerProvider extends BaseProvider implements IRunnerProvid
    * @param parameters workflow job details
    */
   getStepFunctionTask(parameters: RunnerRuntimeParameters): stepfunctions.IChainable {
-    const task = new stepfunctions_tasks.EcsRunTask(
+    return new stepfunctions_tasks.EcsRunTask(
       this,
       this.labels.join(', '),
       {
@@ -484,6 +500,10 @@ export class FargateRunnerProvider extends BaseProvider implements IRunnerProvid
                 value: this.labels.join(','),
               },
               {
+                name: 'RUNNER_GROUP',
+                value: this.group ? `--runnergroup ${this.group}` : '',
+              },
+              {
                 name: 'GITHUB_DOMAIN',
                 value: parameters.githubDomainPath,
               },
@@ -504,8 +524,6 @@ export class FargateRunnerProvider extends BaseProvider implements IRunnerProvid
         ],
       },
     );
-
-    return task;
   }
 
   grantStateMachine(_: iam.IGrantable) {

src/providers/codebuild.ts
@@ -60,6 +60,20 @@ export interface CodeBuildRunnerProviderProps extends RunnerProviderProps {
   readonly labels?: string[];
 
   /**
+   * GitHub Actions runner group name.
+   *
+   * If specified, the runner will be registered with this group name. Setting a runner group can help managing access to self-hosted runners. It
+   * requires a paid GitHub account.
+   *
+   * The group must exist or the runner will not start.
+   *
+   * Users will still be able to trigger this runner with the correct labels. But the runner will only be able to run jobs from repos allowed to use the group.
+   *
+   * @default undefined
+   */
+  readonly group?: string;
+
+  /**
    * VPC to launch the runners in.
    *
    * @default no VPC
@@ -220,6 +234,7 @@ export class CodeBuildRunnerProvider extends BaseProvider implements IRunnerProv
     'CodeBuild.AccountLimitExceededException',
   ];
 
+  private readonly group?: string;
   private readonly vpc?: ec2.IVpc;
   private readonly securityGroups?: ec2.ISecurityGroup[];
   private readonly dind: boolean;
@@ -240,6 +255,7 @@ export class CodeBuildRunnerProvider extends BaseProvider implements IRunnerProv
     }
 
     this.labels = this.labelsFromProperties('codebuild', props?.label, props?.labels);
+    this.group = props?.group;
     this.vpc = props?.vpc;
     if (props?.securityGroup) {
       this.securityGroups = [props.securityGroup];
@@ -274,7 +290,7 @@ export class CodeBuildRunnerProvider extends BaseProvider implements IRunnerProv
             this.dind ? 'nohup dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 --storage-driver=overlay2 &' : '',
             this.dind ? 'timeout 15 sh -c "until docker info; do echo .; sleep 1; done"' : '',
             'if [ "${RUNNER_VERSION}" = "latest" ]; then RUNNER_FLAGS=""; else RUNNER_FLAGS="--disableupdate"; fi',
-            'sudo -Hu runner /home/runner/config.sh --unattended --url "${REGISTRATION_URL}" --token "${RUNNER_TOKEN}" --ephemeral --work _work --labels "${RUNNER_LABEL},cdkghr:started:`date +%s`" ${RUNNER_FLAGS} --name "${RUNNER_NAME}"',
+            'sudo -Hu runner /home/runner/config.sh --unattended --url "${REGISTRATION_URL}" --token "${RUNNER_TOKEN}" --ephemeral --work _work --labels "${RUNNER_LABEL},cdkghr:started:`date +%s`" ${RUNNER_FLAGS} --name "${RUNNER_NAME}" ${RUNNER_GROUP}',
           ],
         },
         build: {
@@ -294,7 +310,7 @@ export class CodeBuildRunnerProvider extends BaseProvider implements IRunnerProv
       buildSpec.phases.install.commands = [
         'cd \\actions',
         'if (${Env:RUNNER_VERSION} -eq "latest") { $RunnerFlags = "" } else { $RunnerFlags = "--disableupdate" }',
-        './config.cmd --unattended --url "${Env:REGISTRATION_URL}" --token "${Env:RUNNER_TOKEN}" --ephemeral --work _work --labels "${Env:RUNNER_LABEL},cdkghr:started:$(Get-Date -UFormat %s)" ${RunnerFlags} --name "${Env:RUNNER_NAME}"',
+        './config.cmd --unattended --url "${Env:REGISTRATION_URL}" --token "${Env:RUNNER_TOKEN}" --ephemeral --work _work --labels "${Env:RUNNER_LABEL},cdkghr:started:$(Get-Date -UFormat %s)" ${RunnerFlags} --name "${Env:RUNNER_NAME}" ${Env:RUNNER_GROUP}',
       ];
       buildSpec.phases.build.commands = [
         'cd \\actions',
@@ -388,6 +404,10 @@ export class CodeBuildRunnerProvider extends BaseProvider implements IRunnerProv
           RUNNER_LABEL: {
             type: codebuild.BuildEnvironmentVariableType.PLAINTEXT,
             value: this.labels.join(','),
+          },
+          RUNNER_GROUP: {
+            type: codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+            value: this.group ? `--runnergroup ${this.group}` : '',
           },
           GITHUB_DOMAIN: {
             type: codebuild.BuildEnvironmentVariableType.PLAINTEXT,

src/providers/ec2.ts
@@ -40,6 +40,7 @@ repoPath="{}"
 runnerTokenPath="{}"
 labels="{}"
 registrationURL="{}"
+runnerGroup="{}"
 
 heartbeat () {
   while true; do
@@ -80,7 +81,7 @@ action () {
   labelsTemplate="$labels,cdkghr:started:$(date +%s)"
 
   # Execute the configuration command for runner registration
-  sudo -Hu runner /home/runner/config.sh --unattended --url "$registrationURL" --token "$runnerTokenPath" --ephemeral --work _work --labels "$labelsTemplate" $RUNNER_FLAGS --name "$runnerNamePath" || exit 1
+  sudo -Hu runner /home/runner/config.sh --unattended --url "$registrationURL" --token "$runnerTokenPath" --ephemeral --work _work --labels "$labelsTemplate" $RUNNER_FLAGS --name "$runnerNamePath" $runnerGroup || exit 1
 
   # Execute the run command
   sudo --preserve-env=AWS_REGION -Hu runner /home/runner/run.sh || exit 2
@@ -113,6 +114,7 @@ $repoPath="{}"
 $runnerTokenPath="{}"
 $labels="{}"
 $registrationURL="{}"
+$runnerGroup="{}"
 
 # EC2Launch only starts ssm agent after user data is done, so we need to start it ourselves (it is disabled by default)
 Set-Service -StartupType Manual AmazonSSMAgent
@@ -148,7 +150,7 @@ function action () {
   cd /actions
   $RunnerVersion = Get-Content /actions/RUNNER_VERSION -Raw
   if ($RunnerVersion -eq "latest") { $RunnerFlags = "" } else { $RunnerFlags = "--disableupdate" }
-  ./config.cmd --unattended --url "\${registrationUrl}" --token "\${runnerTokenPath}" --ephemeral --work _work --labels "\${labels},cdkghr:started:$(Get-Date -UFormat +%s)" $RunnerFlags --name "\${runnerNamePath}" 2>&1 | Out-File -Encoding ASCII -Append /actions/runner.log
+  ./config.cmd --unattended --url "\${registrationUrl}" --token "\${runnerTokenPath}" --ephemeral --work _work --labels "\${labels},cdkghr:started:$(Get-Date -UFormat +%s)" $RunnerFlags --name "\${runnerNamePath}" \${runnerGroup} 2>&1 | Out-File -Encoding ASCII -Append /actions/runner.log
 
   if ($LASTEXITCODE -ne 0) { return 1 }
   ./run.cmd 2>&1 | Out-File -Encoding ASCII -Append /actions/runner.log
@@ -203,6 +205,20 @@ export interface Ec2RunnerProviderProps extends RunnerProviderProps {
    * @default ['ec2']
    */
   readonly labels?: string[];
+
+  /**
+   * GitHub Actions runner group name.
+   *
+   * If specified, the runner will be registered with this group name. Setting a runner group can help managing access to self-hosted runners. It
+   * requires a paid GitHub account.
+   *
+   * The group must exist or the runner will not start.
+   *
+   * Users will still be able to trigger this runner with the correct labels. But the runner will only be able to run jobs from repos allowed to use the group.
+   *
+   * @default undefined
+   */
+  readonly group?: string;
 
   /**
    * Instance type for launched runner instances.
@@ -341,6 +357,7 @@ export class Ec2RunnerProvider extends BaseProvider implements IRunnerProvider {
     'States.Timeout',
   ];
 
+  private readonly group?: string;
   private readonly amiBuilder: IRunnerImageBuilder;
   private readonly ami: RunnerAmi;
   private readonly role: iam.Role;
@@ -357,6 +374,7 @@ export class Ec2RunnerProvider extends BaseProvider implements IRunnerProvider {
     super(scope, id, props);
 
     this.labels = props?.labels ?? ['ec2'];
+    this.group = props?.group;
     this.vpc = props?.vpc ?? ec2.Vpc.fromLookup(this, 'Default VPC', { isDefault: true });
     this.securityGroups = props?.securityGroup ? [props.securityGroup] : (props?.securityGroups ?? [new ec2.SecurityGroup(this, 'SG', { vpc: this.vpc })]);
     this.subnets = props?.subnet ? [props.subnet] : this.vpc.selectSubnets(props?.subnetSelection).subnets;
@@ -422,6 +440,7 @@ export class Ec2RunnerProvider extends BaseProvider implements IRunnerProvider {
       parameters.runnerTokenPath,
       this.labels.join(','),
       parameters.registrationUrl,
+      this.group ? `--runnergroup ${this.group}` : '',
     ];
 
     const passUserData = new stepfunctions.Pass(this, `${this.labels.join(', ')} data`, {

src/providers/ecs.ts
@@ -56,6 +56,20 @@ export interface EcsRunnerProviderProps extends RunnerProviderProps {
   readonly labels?: string[];
 
   /**
+   * GitHub Actions runner group name.
+   *
+   * If specified, the runner will be registered with this group name. Setting a runner group can help managing access to self-hosted runners. It
+   * requires a paid GitHub account.
+   *
+   * The group must exist or the runner will not start.
+   *
+   * Users will still be able to trigger this runner with the correct labels. But the runner will only be able to run jobs from repos allowed to use the group.
+   *
+   * @default undefined
+   */
+  readonly group?: string;
+
+  /**
    * VPC to launch the runners in.
    *
    * @default default account VPC
@@ -321,6 +335,11 @@ export class EcsRunnerProvider extends BaseProvider implements IRunnerProvider {
    */
   private readonly dind: boolean;
 
+  /**
+   * Runner group name.
+   */
+  private readonly group?: string;
+
   readonly retryableErrors = [
     'Ecs.EcsException',
     'ECS.AmazonECSException',
@@ -332,6 +351,7 @@ export class EcsRunnerProvider extends BaseProvider implements IRunnerProvider {
     super(scope, id, props);
 
     this.labels = props?.labels ?? ['ecs'];
+    this.group = props?.group;
     this.vpc = props?.vpc ?? ec2.Vpc.fromLookup(this, 'default vpc', { isDefault: true });
     this.subnetSelection = props?.subnetSelection;
     this.securityGroups = props?.securityGroups ?? [new ec2.SecurityGroup(this, 'security group', { vpc: this.vpc })];
@@ -576,6 +596,10 @@ export class EcsRunnerProvider extends BaseProvider implements IRunnerProvider {
               {
                 name: 'RUNNER_LABEL',
                 value: this.labels.join(','),
+              },
+              {
+                name: 'RUNNER_GROUP',
+                value: this.group ? `--runnergroup ${this.group}` : '',
               },
               {
                 name: 'GITHUB_DOMAIN',

src/providers/lambda-runner.sh
@@ -16,6 +16,7 @@ RUNNER_TOKEN=$(echo "$1" | jq -r .token)
 RUNNER_NAME=$(echo "$1" | jq -r .runnerName)
 RUNNER_LABEL=$(echo "$1" | jq -r .label)
 REGISTRATION_URL=$(echo "$1" | jq -r .registrationUrl)
+RUNNER_GROUP=$(echo "$1" | jq -r .group)
 
 # copy runner code (it needs a writable directory)
 cp -r /home/runner /tmp/
@@ -27,7 +28,7 @@ export HOME=/tmp/home
 
 # start runner
 if [ "${RUNNER_VERSION}" = "latest" ]; then RUNNER_FLAGS=""; else RUNNER_FLAGS="--disableupdate"; fi
-./config.sh --unattended --url "${REGISTRATION_URL}" --token "${RUNNER_TOKEN}" --ephemeral --work _work --labels "${RUNNER_LABEL},cdkghr:started:`date +%s`" --name "${RUNNER_NAME}" ${RUNNER_FLAGS}
+./config.sh --unattended --url "${REGISTRATION_URL}" --token "${RUNNER_TOKEN}" --ephemeral --work _work --labels "${RUNNER_LABEL},cdkghr:started:`date +%s`" --name "${RUNNER_NAME}" ${RUNNER_FLAGS} ${RUNNER_GROUP}
 echo Config done
 ./run.sh
 echo Run done

src/providers/lambda.ts
@@ -60,6 +60,20 @@ export interface LambdaRunnerProviderProps extends RunnerProviderProps {
   readonly labels?: string[];
 
   /**
+   * GitHub Actions runner group name.
+   *
+   * If specified, the runner will be registered with this group name. Setting a runner group can help managing access to self-hosted runners. It
+   * requires a paid GitHub account.
+   *
+   * The group must exist or the runner will not start.
+   *
+   * Users will still be able to trigger this runner with the correct labels. But the runner will only be able to run jobs from repos allowed to use the group.
+   *
+   * @default undefined
+   */
+  readonly group?: string;
+
+  /**
    * The amount of memory, in MB, that is allocated to your Lambda function.
    * Lambda uses this value to proportionally allocate the amount of CPU
    * power. For more information, see Resource Model in the AWS Lambda
@@ -216,6 +230,7 @@ export class LambdaRunnerProvider extends BaseProvider implements IRunnerProvide
     'Lambda.TooManyRequestsException',
   ];
 
+  private readonly group?: string;
   private readonly vpc?: ec2.IVpc;
   private readonly securityGroups?: ec2.ISecurityGroup[];
 
@@ -223,6 +238,7 @@ export class LambdaRunnerProvider extends BaseProvider implements IRunnerProvide
     super(scope, id, props);
 
     this.labels = this.labelsFromProperties('lambda', props?.label, props?.labels);
+    this.group = props?.group;
     this.vpc = props?.vpc;
     this.securityGroups = props?.securityGroup ? [props.securityGroup] : props?.securityGroups;
 
@@ -325,6 +341,7 @@ export class LambdaRunnerProvider extends BaseProvider implements IRunnerProvide
           owner: parameters.ownerPath,
           repo: parameters.repoPath,
           registrationUrl: parameters.registrationUrl,
+          group: this.group ? `--runnergroup ${this.group}` : '',
         }),
       },
     );

commit b4f885128298e37969acb64b8a2445184e1a7727

fix: Windows Fargate/ECS stopped working due to runner groups (#634)
This issue was never present in a release. It was caused by #630.

src/providers/fargate.ts
@@ -236,7 +236,7 @@ export function ecsRunCommand(os: Os, dind: boolean): string[] {
       'powershell', '-Command',
       `cd \\actions ;
         if ($Env:RUNNER_VERSION -eq "latest") { $RunnerFlags = "" } else { $RunnerFlags = "--disableupdate" } ;
-        ./config.cmd --unattended --url "\${Env:REGISTRATION_URL}" --token "\${Env:RUNNER_TOKEN}" --ephemeral --work _work --labels "\${Env:RUNNER_LABEL},cdkghr:started:\$(Get-Date -UFormat +%s)" $RunnerFlags --name "\${Env:RUNNER_NAME} \${Env:RUNNER_GROUP}" ;
+        ./config.cmd --unattended --url "\${Env:REGISTRATION_URL}" --token "\${Env:RUNNER_TOKEN}" --ephemeral --work _work --labels "\${Env:RUNNER_LABEL},cdkghr:started:\$(Get-Date -UFormat +%s)" $RunnerFlags --name "\${Env:RUNNER_NAME}" \${Env:RUNNER_GROUP} ;
         ./run.cmd ;
         $STATUS = Select-String -Path './_diag/*.log' -Pattern 'finish job request for job [0-9a-f\\-]+ with result: (.*)' | %{$_.Matches.Groups[1].Value} | Select-Object -Last 1 ;
         if ($STATUS) { echo "CDKGHA JOB DONE $\{Env:RUNNER_LABEL\} $STATUS" }`,