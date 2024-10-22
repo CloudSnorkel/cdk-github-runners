f5a868ac5f4be38a050cb17e3d350f8e5240ec4c
Amir Szekely <kichik@gmail.com>
2024-10-22 10:10:22 -0400

    chore: Upgrade to TypeScript 5.6 and Svelte 5 (#639)

--- setup/src/main.ts
+++ setup/src/main.ts
@@ -1,8 +1,7 @@
 import './app.scss';
+import { mount } from 'svelte';
 import App from './App.svelte';
 
-const app = new App({
-  target: document.getElementById('app'),
-});
+const app = mount(App, { target: document.getElementById('app')! });
 
 export default app;
