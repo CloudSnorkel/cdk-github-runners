f959afa3db2329b45e5bf0d852c7da74d88d9f47
Amir Szekely <kichik@gmail.com>
2025-12-22 22:18:07 -0500

    lines too long for eslint

--- setup/src/App.svelte
+++ setup/src/App.svelte
@@ -320,7 +320,8 @@
               Would you like runners to be registered on repository level, or on organization level?
             </p>
             <p>
-              <em>This determines where runners will be registered dynamically each time they are provisioned. This is independent of where the GitHub app is installed.</em>
+              <em>This determines where runners will be registered dynamically each time they are provisioned. This is
+                independent of where the GitHub app is installed.</em>
             </p>
             <ul>
               <li>
@@ -333,13 +334,18 @@
                 which is more fine-grained.
               </li>
               <li>
-                <strong>Repository level is recommended</strong> because runners are registered to the specific repository where the job started, preventing jobs from being assigned to runners intended for other repositories.
+                <strong>Repository level is recommended</strong> because runners are registered to the specific repository
+                where the job started, preventing jobs from being assigned to runners intended for other repositories.
               </li>
               <li>
-                Registering runners on organization level means runners are registered to the entire organization, making them available to <strong>ALL repositories in the organization</strong>, regardless of whether the app is installed on those repositories. This can lead to jobs being routed to runners that weren't intended for them.
+                Registering runners on organization level means runners are registered to the entire organization, making
+                them available to <strong>ALL repositories in the organization</strong>, regardless of whether the app is
+                installed on those repositories. This can lead to jobs being routed to runners that weren't intended for them.
               </li>
               <li>
-                Organization level registration does <strong>not</strong> automatically install the app on all repositories. You still need to install the app on each repository where you want to use runners. GitHub will ask you on which repositories to install this app after it's created.
+                Organization level registration does <strong>not</strong> automatically install the app on all repositories.
+                You still need to install the app on each repository where you want to use runners. GitHub will ask you
+                on which repositories to install this app after it's created.
               </li>
               <li>
                 Do not use organization level registration if you don't fully trust all repositories in your organization.
