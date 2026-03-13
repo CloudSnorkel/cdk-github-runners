f7e273ef53c1dd57dd044de3a9a639325c71c113
Amir Szekely <kichik@gmail.com>
2026-03-12 21:50:09 -0400

    feat: GPU support (#907)
    Add base images with GPU drivers for EC2 with `BaseImage.fromGpuBase()`. Add GPU support for CodeBuild and ECS providers.
    
    EC2 already supported GPU by specifying an instance type that has a GPU, but this should make getting drivers/CUDA installed easier.
    
    ### EC2 Example
    
    ```typescript
    import { Size } from 'aws-cdk-lib';
    import { InstanceClass, InstanceSize, InstanceType } from 'aws-cdk-lib/aws-ec2';
    import { Ec2RunnerProvider, GitHubRunners } from '@cloudsnorkel/cdk-github-runners';
    
    new GitHubRunners(this, 'Runners', {
      providers: [
        new Ec2RunnerProvider(this, 'GPU Provider', {
          labels: ['gpu'],
          instanceType: InstanceType.of(InstanceClass.G4DN, InstanceSize.XLARGE),
          storageSize: Size.gibibytes(100), // deep learning AMI is larger than default 30GB
        }),
      ],
    });
    
    // if you need a custom image builder use:
    
    const imageBuilder = Ec2RunnerProvider.imageBuilder(this, 'EC2 GPU Image Builder', {
      baseAmi: BaseImage.fromGpuBase(Os.LINUX_UBUNTU_2204, Architecture.X86_64),
    });
    ```
    
    ### ECS Example
    
    ```typescript
    import { BaseContainerImage, EcsRunnerProvider, GitHubRunners, Os } from '@cloudsnorkel/cdk-github-runners';
    
    new GitHubRunners(this, 'Runners', {
      providers: [
        new EcsRunnerProvider(this, 'GPU Provider', {
          labels: ['gpu'],
          gpu: 1,
          imageBuilder: EcsRunnerProvider.imageBuilder(this, 'ECS GPU Image Builder', {
            os: Os.LINUX_UBUNTU_2204,
            baseDockerImage: BaseContainerImage.fromEcrPublic('deep-learning-containers', 'base', '13.0.2-gpu-py313-ubuntu22.04-ec2'),
          }),
        }),
      ],
    });
    ```
    
    ### CodeBuild Example
    
    ```typescript
    import { BaseContainerImage, CodeBuildRunnerProvider, GitHubRunners, Os } from '@cloudsnorkel/cdk-github-runners';
    
    new GitHubRunners(this, 'Runners', {
      providers: [
        new CodeBuildRunnerProvider(this, 'GPU Provider', {
          labels: ['gpu'],
          gpu: true,
          imageBuilder: CodeBuildRunnerProvider.imageBuilder(this, 'CodeBuild GPU Image Builder', {
            os: Os.LINUX_UBUNTU_2204,
            baseDockerImage: BaseContainerImage.fromEcrPublic('deep-learning-containers', 'base', '13.0.2-gpu-py313-ubuntu22.04-ec2'),
          });
        }),
      ],
    });
    ```
    
    Closes #54

--- src/providers/ec2.ts
+++ src/providers/ec2.ts
@@ -34,7 +34,8 @@ import {
   RunnerImageBuilderType,
   RunnerImageComponent,
 } from '../image-builders';
-import { MINIMAL_EC2_SSM_SESSION_MANAGER_POLICY_STATEMENT } from '../utils';
+import { BaseImage } from '../image-builders/aws-image-builder';
+import { isGpuInstanceType, MINIMAL_EC2_SSM_SESSION_MANAGER_POLICY_STATEMENT } from '../utils';
 
 // this script is specifically made so `poweroff` is absolutely always called
 // each `{}` is a variable coming from `params` below
@@ -239,6 +240,11 @@ export interface Ec2RunnerProviderProps extends RunnerProviderProps {
   /**
    * Instance type for launched runner instances.
    *
+   * For GPU instance types (g4dn, g5, p3, etc.), we automatically use a GPU base image (AWS Deep Learning AMI)
+   * with NVIDIA drivers pre-installed. If you provide your own image builder, use
+   * `baseAmi: BaseImage.fromGpuBase(os, architecture)` or another image preloaded with NVIDIA drivers, or use
+   * an image component to install NVIDIA drivers.
+   *
    * @default m6i.large
    */
   readonly instanceType?: ec2.InstanceType;
@@ -410,6 +416,7 @@ export class Ec2RunnerProvider extends BaseProvider implements IRunnerProvider {
       vpc: props?.vpc,
       subnetSelection: props?.subnetSelection,
       securityGroups: this.securityGroups,
+      baseAmi: isGpuInstanceType(this.instanceType) ? BaseImage.fromGpuBase(Os.LINUX_UBUNTU, arch) : undefined,
       architecture: arch,
       awsImageBuilderOptions: {
         instanceType: arch.is(Architecture.ARM64) ? ec2.InstanceType.of(ec2.InstanceClass.M6G, ec2.InstanceSize.LARGE) : undefined,
