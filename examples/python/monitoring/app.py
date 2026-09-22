f89fcf0b6e795d186365138eb003f35f92ad3c75
Amir Szekely <kichik@gmail.com>
2026-09-21 21:15:27 -0400

    fix examples

--- examples/python/monitoring/app.py
+++ examples/python/monitoring/app.py
@@ -40,6 +40,9 @@ class MonitoringStack(Stack):
             alarm_description="Alert when runner starts fail"
         )
 
+        # Create a CloudWatch dashboard with the most useful runner metrics in one place
+        runners.createDashboard()
+
         # Notify us when runner image builds fail
         # Runner images are rebuilt every week by default. Failed builds mean you'll get
         # stuck with out-of-date software, which may lead to security vulnerabilities
